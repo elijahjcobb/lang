d8594d82022dd4f94298bd15eb1d2d9d34ff4fba
Elijah Cobb <elijah@elijahcobb.com>
2024-01-05 21:39:47 -0800

    add func decl

--- src/lexar/functions/declaration.test.ts
+++ src/lexar/functions/declaration.test.ts
@@ -2,8 +2,58 @@ import { lexar } from "..";
 
 describe("variables", () => {
   it("works on normal value", () => {
-    console.log(
-      JSON.stringify(lexar(`fun add(a, b) { 1 + a; a + b - 1 * c; }`), null, 4)
-    );
+    expect(lexar(`fun add(a, b) { a + b - 1 * 2389 }`)).toEqual({
+      statements: [
+        {
+          type: "function-declaration",
+          name: "add",
+          arguments: [
+            {
+              type: "argument",
+              name: "a",
+            },
+            {
+              type: "argument",
+              name: "b",
+            },
+          ],
+          body: [
+            {
+              type: "binary-expression",
+              expression: "multiplication",
+              left: {
+                type: "binary-expression",
+                expression: "addition",
+                left: {
+                  type: "variable",
+                  name: "a",
+                },
+                right: {
+                  type: "binary-expression",
+                  expression: "subtraction",
+                  left: {
+                    type: "variable",
+                    name: "b",
+                  },
+                  right: {
+                    type: "literal",
+                    literalType: "integer",
+                    value: 1,
+                  },
+                },
+              },
+              right: {
+                type: "literal",
+                literalType: "integer",
+                value: 2389,
+              },
+            },
+          ],
+        },
+      ],
+      context: {
+        heap: {},
+      },
+    });
   });
 });
